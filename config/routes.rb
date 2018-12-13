6153deb30c92554163dc9708c60bd30577ed4e0d
高橋英明 <kaenriter.manzai@gamil.com>
2018-12-13 19:18:47 +0900

    edit_route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    get "/signup" => "devise/registrations#new", as: "new_user_registration"
+    get "/signup/registration" => "devise/registrations#registration"
+    get "/signup/sms_confirmation" => "devise/registrations#sms_confirmation"
+    get "/signup/address" => "devise/registrations#address"
+    get "/signup/credit_card" => "devise/registrations#credit_card"
+    get "/signup/done" => "devise/registrations#done"
+  end
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
