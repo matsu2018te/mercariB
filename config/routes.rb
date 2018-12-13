09bda84f6e204ce32f41c369dea091d649a934db
hiroshimuto <hiroshi.github@gmail.com>
2018-12-13 11:44:24 +0900

    Create User Controller(default actions)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'mypage' => 'users#show'
+  get 'mypage/profile' => 'users#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
