62df916569f99e4958c98bef5a3cf0c334046d8f
hiroshimuto <hiroshi.github@gmail.com>
2018-12-13 14:58:32 +0900

    Install Rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true, length: { minimum: 6 }, confirmation: true
-  validates :telephone, presence: true, format: { with: VALID_PHONE_REGEX }
-  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
-
-
-
+  validates :telephone, presence: true
 end
