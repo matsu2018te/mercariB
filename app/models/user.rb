c051dd63f275994330766113e02ca4f6eb54f16a
hiroshimuto <hiroshi.github@gmail.com>
2018-12-13 14:08:13 +0900

    set user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, confirmation: true
+  validates :telephone, presence: true, format: { with: VALID_PHONE_REGEX }
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+
+
+
 end
