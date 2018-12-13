67a58cbab842f89281a83c0ad8a74a2e96f1aa7e
matsu2018te <hiroyukimatsumoto@MacBook-Air-HM.local>
2018-12-13 16:50:44 +0900

    New user registration, login
    【サーバサイド】ユーザー新規登録、ログイン(メールアドレスのみ)

--- Gemfile
+++ Gemfile
@@ -43,6 +43,11 @@ group :development, :test do
   gem 'pry-doc'
   gem 'rspec-rails'
   gem 'factory_girl_rails', "~> 4.4.1"
+  gem 'capistrano'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano3-unicorn'
 end
 
 group :development do
@@ -65,3 +70,7 @@ gem 'erb2haml'
 gem 'font-awesome-rails'
 # Devise
 gem 'devise'
+group :production do
+  #Unicorn
+  gem 'unicorn'
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
   private
 
@@ -8,5 +9,16 @@ class ApplicationController < ActionController::Base
     added_attrs = [ :nickname, :first_name, :last_name, :fist_name_phonetic, :last_name_phonetic, :telephone, :birthday, :icon_picture, :profile, :background_image, :point]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+
   end
 end
