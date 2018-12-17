3e6e5c70b294cdff6941942e9a6c17335ab869b5
高橋英明 <kaenriter.manzai@gamil.com>
2018-12-17 20:50:34 +0900

    edit_table

---
+++ db/migrate/20181217114434_remove_products_from_table.rb
@@ -0,0 +1,5 @@
+class RemoveProductsFromTable < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :products
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181216032957) do
+ActiveRecord::Schema.define(version: 20181217114434) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "postal_code",    null: false
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20181216032957) do
     t.integer  "birth_year",                                        null: false
     t.integer  "birth_month",                                       null: false
     t.integer  "birth_day",                                         null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "token"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
