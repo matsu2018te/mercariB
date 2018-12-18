22717c74b106f8b6e2c37b84a4a0e8f91ca21cc4
高橋英明 <kaenriter.manzai@gamil.com>
2018-12-18 21:11:20 +0900

    add_create_method

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -3,11 +3,18 @@ class CreditsController < ApplicationController
   end
 
   def new
+    @credit = Credit.new
   end
 
   def create
+    Credit.create(credit_params)
   end
 
   def destroy
+  end
+
+  private
+  def credit_params
+    params.require(:credit).permit(:card_number, :security_code, :expiration_month, :expiration_year).merge(user_id: current_user.id)
   end
 end
