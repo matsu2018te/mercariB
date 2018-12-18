dc2495abd35a7bd3583c90003c27f545011846c7
高橋英明 <kaenriter.manzai@gamil.com>
2018-12-18 12:39:10 +0900

    add_iden_edit

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   get 'mypage' => 'users#show'
+  post 'mypage' => 'users#update'
   get 'mypage/profile' => 'users#edit'
+  get 'mypage/identification' => 'users#iden_edit'
   get 'mypage/logout' => 'users#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
