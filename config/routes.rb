b9ace73fe345122b9631dafee10bb1c27b782a24
hiroshimuto <hiroshi.github@gmail.com>
2018-12-19 21:32:54 +0900

    Create devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
     get 'login' => 'devise/sessions#new'#ログイン
     get 'signup' => 'devise/registrations#signup' #新規会員登録
     get "/signup/registration" => "devise/registrations#new"#会員情報入力
-    get "/signup/sms_confirmation" => "devise/registrations#sms_confirmation"#電話番号入力
-    get "/signup/address" => "devise/registrations#address"#住所入力
-    get "/signup/credit_card" => "devise/registrations#credit_card"#支払い方法
-    get "/signup/done" => "devise/registrations#done"#完了画面
+    post "/signup/sms_confirmation" => "devise/registrations#sms_confirmation"#電話番号入力
+    post "/signup/address" => "devise/registrations#address"#住所入力
+    post "/signup/credit_card" => "devise/registrations#credit_card"#支払い方法
+    post "/signup/done" => "devise/registrations#edit"#完了画面
   end
 
 
