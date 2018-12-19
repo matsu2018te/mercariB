49012844799a538d6c1b0a436787c74ef14658b1
matsu2018te <hiroyukimatsumoto@MacBook-Air-HM.local>
2018-12-19 19:16:37 +0900

    Productテーブルのバリデーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,6 @@ class Product < ApplicationRecord
   belongs_to :sell_status
   has_many :images
   accepts_nested_attributes_for :images
+
+  validates :seller, :category, :area, :sell_status, presence: true
 end
