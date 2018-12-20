e5143ea310b831a4f913d67e5db41a75f2255d04
knotchy <tkei.no303@gmail.com>
2018-12-20 18:50:55 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,12 @@ class ProductsController < ApplicationController
   def new
 
   end
+
+  def create
+
+  end
+
+  def products_params
+    params.require(:products).permit(:product_name, :product_info, :price, :category_id, :image).merge(seller_id: current_user.id)
+  end
 end
