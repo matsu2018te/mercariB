5c1130cbe8b3eef624b258683a90c5cede16ec85
hiroshimuto <hiroshi.github@gmail.com>
2018-12-20 17:22:20 +0900

    delete space

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,10 +4,8 @@ class Address < ApplicationRecord
   #都道府県
   include JpPrefecture
   jp_prefecture :prefecture, method_name: :pref
-
   validates :postal_code, presence: true
   validates :prefecture, presence: true
   validates :municipality, presence: true
   validates :address_number, presence: true
-
 end
