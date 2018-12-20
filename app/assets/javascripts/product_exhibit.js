4cf5089e0fa1788ffb05571938de309b91466026
knotchy <tkei.no303@gmail.com>
2018-12-20 21:22:18 +0900

    Update product_exhibit.js

--- app/assets/javascripts/product_exhibit.js
+++ app/assets/javascripts/product_exhibit.js
@@ -14,9 +14,9 @@ $(document).on('turbolinks:load', function() {
       var fee = Math.floor(price / 10);
       var gains = Math.floor(price - fee);
       var fee_num = ReplaceNum(fee)
-      $(".sell-form-price-fee-num").text(fee_num);
+      $(".sell-form-price-fee-num").text('¥ ' + fee_num);
       var gains_num = ReplaceNum(gains)
-      $(".sell-form-price-gains-num").text(gains_num);
+      $(".sell-form-price-gains-num").text('¥ ' + gains_num);
       }
     else {
       $(".sell-form-price-fee-num").text('-');
