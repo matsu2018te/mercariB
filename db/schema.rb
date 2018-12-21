0ee5c40bed0c25e9e117bf3e3d04a7af95c21897
高橋英明 <kaenriter.manzai@gamil.com>
2018-12-21 20:07:37 +0900

    rename_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181221021705) do
+ActiveRecord::Schema.define(version: 20181221110401) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "postal_code",    null: false
@@ -31,11 +31,11 @@ ActiveRecord::Schema.define(version: 20181221021705) do
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "category_name", null: false
+    t.string   "name",       null: false
     t.string   "belongs"
     t.string   "ancestry"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
@@ -61,10 +61,10 @@ ActiveRecord::Schema.define(version: 20181221021705) do
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "seller_id"
     t.integer  "buyer_id"
-    t.string   "product_name",                     null: false
-    t.text     "product_info",       limit: 65535, null: false
+    t.string   "name",                             null: false
+    t.text     "info",               limit: 65535, null: false
     t.integer  "price",                            null: false
-    t.string   "product_state",                    null: false
+    t.string   "status",                           null: false
     t.string   "delivery_fee_owner",               null: false
     t.datetime "delivery_date",                    null: false
     t.string   "shipping_method",                  null: false
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 20181221021705) do
     t.integer  "brand_id"
     t.integer  "sell_status_id"
     t.integer  "size_id"
-    t.integer  "area",                             null: false
     t.index ["brand_id"], name: "index_products_on_brand_id", using: :btree
     t.index ["buyer_id"], name: "index_products_on_buyer_id", using: :btree
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
@@ -91,6 +90,15 @@ ActiveRecord::Schema.define(version: 20181221021705) do
 
   create_table "sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "size", null: false
+  end
+
+  create_table "sns_credentials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "uid"
+    t.string   "provider"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -129,4 +137,5 @@ ActiveRecord::Schema.define(version: 20181221021705) do
   add_foreign_key "products", "sizes"
   add_foreign_key "products", "users", column: "buyer_id"
   add_foreign_key "products", "users", column: "seller_id"
+  add_foreign_key "sns_credentials", "users"
 end
