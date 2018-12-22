21872253d9f8e7baced880f20a7b0c339aa0ef16
高橋英明 <kaenriter.manzai@gamil.com>
2018-12-22 18:40:31 +0900

    Merge branch 'master' into add_reCAPTCHA
    # Conflicts:
    #	app/views/devise/registrations/new.html.haml

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
-  belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id'
+  belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id', optional: true
   belongs_to :seller, class_name: 'User', :foreign_key => 'seller_id'
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :category
   belongs_to :brand, optional: true
   belongs_to :size
@@ -9,5 +9,5 @@ class Product < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images
 
-  validates :seller, :category, :area, :sell_status, presence: true
+  validates :seller, :category, :sell_status, presence: true
 end
