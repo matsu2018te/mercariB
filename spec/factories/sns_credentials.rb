0426815b8ef408b9005e8131e8e109ab99d65d9d
高橋英明 <kaenriter.manzai@gamil.com>
2018-12-23 20:11:00 +0900

    edit_test

--- spec/factories/sns_credentials.rb
+++ spec/factories/sns_credentials.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
     factory :sns_credential do
-        email "rspec_test@test.com"
-        uid "1111111111"
-        provider "facebook"
-        username "anonymous"
-        confirmed_at Time.now
+      email "merucari@gmail.com"
+      uid "1111111111"
+      provider "facebook"
+      confirmed_at Time.now
     end
 end
--- spec/requests/sns_credential.rb
+++ spec/requests/sns_credential.rb
@@ -1,30 +1,47 @@
 require 'rails_helper'
 
-describe "#self.from_omniauth" do
-  before do
-    OmniAuth.config.test_mode = true
-  end
-
-  after do
-    OmniAuth.config.test_mode = false
-  end
-
-  let(:auth){
+def facebook_mock
+  OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(
     {
-      "provider" => "test_provider",
-      "uid" => "test_uid",
-      "info" => { "nickname" => "testuser" }
+      provider: 'facebook',
+      uid: '12345',
+      info: {
+        name: 'mockuser',
+        email: 'merucari@gmail.com'
+      },
     }
-  }
-  context "when user is not created" do
-    it "succeeded in creating user." do
-      expect{ User.find_oauth(auth) }.to change{ User.count }.by(1)
+  )
+end
+
+describe "UserFeature" do
+  describe "facebook連携でサインアップする" do
+    before do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:facebook] = nil
+      Rails.application.env_config['omniauth.auth'] = facebook_mock
+      visit root_path
     end
-  end
-  context "when user has already been created" do
-    it "create " do
-      user = build(:user)
-      expect{ User.find_oauth(auth) }.not_to change{ User.count }
+
+    after do
+      OmniAuth.config.test_mode = false
+    end
+
+    it "サインアップするとユーザーが増える" do
+      click_on "新規会員登録"
+      expect{
+        click_link "Facebookで登録"
+      }.to change(User, :count).by(1)
+    end
+
+    it "すでに連携されたユーザーがサインアップしようとするとユーザーは増えない" do
+      click_on "新規会員登録"
+      click_link "Facebookで登録"
+      visit mypage_logout_path
+      click_link "ログアウト", match: :first
+      click_link "ログイン"
+      expect{
+        click_link "Facebookでログイン"
+      }.not_to change(User, :count)
     end
   end
 end
