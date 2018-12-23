534cee922460e7f9e9af11c82d4d8791b93fa756
matsu2018te <hiroyukimatsumoto@MacBook-Air-HM.local>
2018-12-23 13:56:08 +0900

    雛形作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
     @products = Product.order("id DESC").limit(4)
+    @ladies = @products.where(category_id: '').limit(4)
+    @mens = @products.where(category_id: '').limit(4)
+    @baby = @products.where(category_id: '').limit(4)
+    @cosme = @products.where(category_id: '').limit(4)
+    @chanel = @products.where(brand: '').limit(4)
+    @louisvuitton = @products.where(brand: '').limit(4)
+    @supreme = @products.where(brand: '').limit(4)
+    @nike = @products.where(brand: '').limit(4)
   end
 end
