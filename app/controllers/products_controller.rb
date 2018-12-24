0213ab21dfc105de735bf9309868deacbc8251f3
hiroshimuto <hiroshi.github@gmail.com>
2018-12-24 12:08:57 +0900

    debug variables

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @images = @product.images
     @sell_user = @product.seller
-    @sell_items = Product.where(seller_id: @product.seller_id)
+    @sell_other_products = Product.where(seller_id: @product.seller_id)
     @sell_product_brand = Brand.find_by(id: @product.brand_id)
     @sell_product_category = Category.find_by(id: @product.category_id)
 
@@ -16,6 +16,8 @@ class ProductsController < ApplicationController
     else @product.brand_id.present? || @product.category_id.present?
       @related_items = Product.where("brand_id = ? or category_id = ?", @product.brand_id, @product.category_id)
     end
+    # binding.pry
+
   end
 
     def transaction
