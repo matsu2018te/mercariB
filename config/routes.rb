efba59430b81bbd4d31e2b1377a7e205f315df89
高橋英明 <kaenriter.manzai@gamil.com>
2018-12-24 16:32:52 +0900

    edit_route

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,14 @@ Rails.application.routes.draw do
   },
   skip: [:sessions, :registrations]
   as :user do
-    get 'login' => 'devise/sessions#new', as: :new_user_session#ログイン
+    #ログイン
+    get 'login' => 'devise/sessions#new', as: :new_user_session
     post 'login' => 'devise/sessions#create', as: :user_session
+    #ログアウト
     match 'mypage/logout' => 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
-    get 'signup' => 'users/registrations#signup' #新規会員登録
+    get 'mypage/logout' => 'users#destroy'
+    #サインアップ
+    get 'signup' => 'users/registrations#signup'#新規会員登録
     get "/signup/registration" => "users/registrations#registration"#会員情報入力
     post "/signup/sms_confirmation" => "users/registrations#sms_confirmation"#電話番号入力
     post "/signup/address" => "users/registrations#address"#住所入力
@@ -28,20 +32,20 @@ Rails.application.routes.draw do
     post 'card' => 'credits#create'
   end
 
+  #user関連
   get 'mypage' => 'users#show'
   patch 'mypage' => 'users#update'
   get 'mypage/profile' => 'users#edit'
   get 'mypage/identification' => 'users#set_user'
   get 'mypage/notification' => 'users#notification'
   get 'mypage/todo' => 'users#todo'
+
+  #products関連
+  resources :products
   get 'mypage/purchase' => 'products#purchase'
   get 'mypage/purchased' => 'products#purchased'
-  get 'mypage/logout' => 'users#destroy'
   get 'sell' => 'products#new'
   get 'transaction' => 'products#transaction'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :products
 end
 
 
