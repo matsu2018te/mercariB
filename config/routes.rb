545b71f30236e477ab4607499058ad89b5f160c0
knotchy <tkei.no303@gmail.com>
2018-12-25 20:56:04 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -32,11 +32,12 @@ Rails.application.routes.draw do
   get 'mypage/logout' => 'users#destroy'
   get 'show' => 'products#show'
   get 'sell' => 'products#new'
-  post 'sell' => 'product#create'
+  post 'sell' => 'products#create'
   get 'mypage/card' => 'credits#index'
   get 'mypage/card/create' => 'credits#new'
   post 'mypage/card' => 'credits#create'
   delete 'mypage/card' => 'credits#destroy'
+  get 'brand/index' => 'brands#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
