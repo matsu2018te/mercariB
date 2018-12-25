bb246a1b27c1a7de69314ef33520e1914c67b231
matsu2018te <hiroyukimatsumoto@MacBook-Air-HM.local>
2018-12-25 19:07:39 +0900

    ピックアップカテゴリー欄修正
    レディース、メンズ、ベビー・キッズ、コスメ
    各カテゴリー毎に商品を表示するようビューファイルとコントローラを修正。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,26 @@
 class HomeController < ApplicationController
   def index
     @products = Product.order("id DESC").limit(4)
-    @ladies = @products.where(category_id: '').limit(4)
-    @mens = @products.where(category_id: '').limit(4)
-    @baby = @products.where(category_id: '').limit(4)
-    @cosme = @products.where(category_id: '').limit(4)
-    @chanel = @products.where(brand: '').limit(4)
-    @louisvuitton = @products.where(brand: '').limit(4)
-    @supreme = @products.where(brand: '').limit(4)
-    @nike = @products.where(brand: '').limit(4)
+
+    ladies = Category.where("ancestry LIKE ?", "1/%")
+    ladies_ids = ladies.map{|lady| lady.id }
+    @lady_products = Product.where(category_id:ladies_ids).limit(4)
+
+    mens = Category.where("ancestry LIKE ?", "2/%")
+    mens_ids = mens.map{|mens| mens.id }
+    @mens_products = Product.where(category_id:mens_ids).limit(4)
+
+    kids = Category.where("ancestry LIKE ?", "3/%")
+    kids_ids = kids.map{|kids| kids.id }
+    @kids_products = Product.where(category_id:kids_ids).limit(4)
+
+    cosme = Category.where("ancestry LIKE ?", "7/%")
+    cosme_ids = cosme.map{|cosme| cosme.id }
+    @cosme_products = Product.where(category_id:cosme_ids).limit(4)
+
+    # @louisvuitton = @productss.brand.where(name: 'スタークインダストリーズ').limit(4)
+    # @supreme = @products.brands.where(name: 'ウルヴァリン').limit(4)
+    # @nike = @products.brands.where(name: 'ウルヴァリン').limit(4)
   end
 end
+
