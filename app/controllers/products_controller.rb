4b640b703e06efc3793ba68c1d5c3bd3915c772c
matsu2018te <hiroyukimatsumoto@MacBook-Air-HM.local>
2018-12-26 16:16:13 +0900

    クレジット決済を行う処理を追記

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,9 +20,21 @@ class ProductsController < ApplicationController
 
   end
 
-    def transaction
-      @product = Product.find(params[:id])
-      @product.update(buyer_id: current_user.id)
-    end
+  def transaction
+    @product = Product.find(params[:format])
+  end
+
+  def completed_transaction
+    @product = Product.find(params[:id])
+    require 'payjp'
+    Payjp.api_key = PAYJP_SECRET_KEY
+
+    Payjp::Charge.create(
+      amount:  @product.price,
+      card:    params['payjp-token'],
+      currency: 'jpy',
+    )
+    @product.update(buyer_id: current_user.id)
+  end
 
 end
