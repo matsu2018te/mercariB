01df4bbb23277211dcc5a72abb57af635061930d
knotchy <tkei.no303@gmail.com>
2019-01-07 12:03:22 +0900

    Update db:images

--- db/migrate/20181227100406_add_column_to_images.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnToImages < ActiveRecord::Migration[5.0]
-  def change
-    add_column :images, :image2, :string, after: :image
-    add_column :images, :image3, :string, after: :image2
-    add_column :images, :image4, :string, after: :image3
-  end
-end
