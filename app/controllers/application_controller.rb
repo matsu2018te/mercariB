58b7da3bc20de72b70e288f8ca9f8e4715ba66f5
matsu2018te <hiroyukimatsumoto@MacBook-Air-HM.local>
2019-01-08 15:07:17 +0900

    コントローラ追加修正
    カテゴリー、ブランド毎に検索するためのcategoriesコントローラを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
+  before_action :category_search
 
   private
 
@@ -21,4 +22,10 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '2222'
     end
   end
+
+  def category_search
+    @parents = Category.where(belongs: "parent")
+    @brands = Brand.all
+  end
+
 end
