87537ed5b810823179c35bf8c4f9cb2f995fe7b0
高橋英明 <kaenriter.manzai@gamil.com>
2019-01-09 10:36:09 +0900

    js_refactoring

--- app/assets/javascripts/search_select.js
+++ app/assets/javascripts/search_select.js
@@ -1,48 +1,48 @@
 $(function(){
-  function append_child_option(results){
-    var child_select = $(".child_select")
-    var check_boxes = $(".g_child_check_boxes")
-    child_select.empty()
+  function append_child_option(select, check_boxes, results){
+    select.empty()
     check_boxes.empty();
-    child_select.append(`<option value="">---</option>`);
+    select.append(`<option value="">---</option>`);
     results.forEach(function(result){
-      child_select.append(`<option value="${result.id}">${result.name}</option>`)
+      select.append(`<option value="${result.id}">${result.name}</option>`)
     })
   }
 
-  function append_g_child_check_boxes(results){
-    var check_boxes = $(".g_child_check_boxes")
+  function append_g_child_check_boxes(check_boxes, results){
     check_boxes.empty();
     results.forEach(function(result){
       check_boxes.append(`<div  class="form__checkbox">\n\t<input type="checkbox" value="${result.id}" name="q[category_id_in][]" id="q_category_id_in_${result.id}">\n\t<label for="q_category_id_in_${result.id}">${result.name}</label>\n</div>`)
     })
   }
 
-  function append_size_check_boxes(results) {
-    var check_boxes = $(".size_check_boxes")
-    check_boxes.empty();
+  function append_size_check_boxes(check_box, results) {
+    check_box.empty();
     results.forEach(function(result){
-      check_boxes.append(`<div class="form__checkbox">\n<input type="checkbox" value="${result.id}" name="q[size_id_in][]" id="q_size_id_in_${result.id}">\n<label for="q_size_id_in_${result.id}">${result.size}</label>\n</div>`)
+      check_box.append(`<div class="form__checkbox">\n<input type="checkbox" value="${result.id}" name="q[size_id_in][]" id="q_size_id_in_${result.id}">\n<label for="q_size_id_in_${result.id}">${result.size}</label>\n</div>`)
     })
   }
 
-  $('.parent_select').change(function() {
+  var child_select = $(".child_select")
+  var g_child_check_boxes = $(".g_child_check_boxes")
+  var parent_select = $('.parent_select')
+  var hide_wrap = $(".hide-wrap")
+  parent_select.change(function() {
     var parent_val = $(this).val();
     if ($.isEmptyObject(parent_val)){
-      $(".hide-wrap").hide();
+      hide_wrap.hide();
     } else {
-      $(".hide-wrap").show();
+      hide_wrap.show();
     }
     var parent_results = gon.children.filter(function(e){
       if (e.ancestry == parent_val){
         return e
       }
     })
-    append_child_option(parent_results);
+    append_child_option(child_select, g_child_check_boxes, parent_results);
   });
 
-  $('.child_select').change(function() {
-    var parent_val = $('.parent_select').val();
+  child_select.change(function() {
+    var parent_val = parent_select.val();
     var child_val = $(this).val();
     var ancestry = `${parent_val}/${child_val}`
     var child_results = gon.g_children.filter(function(e){
@@ -50,20 +50,23 @@ $(function(){
         return e
       }
     })
-    append_g_child_check_boxes(child_results);
+    append_g_child_check_boxes(g_child_check_boxes,child_results);
   });
 
-  $('.size_group_select').change(function() {
+  var size_check_boxes = $(".size_check_boxes")
+  var size_group_select = $('.size_group_select')
+  size_group_select.change(function() {
     var size_group_val = $(this).val();
     var size_group_results = gon.sizes.filter(function(e) {
       if (e.size_group_id == size_group_val){
         return e
       }
     });
-    append_size_check_boxes(size_group_results);
+    append_size_check_boxes(size_check_boxes, size_group_results);
   });
 
-  $('.price_select').change(function(){
+  var price_select = $('.price_select')
+  price_select.change(function(){
     var price_select_val = $(this).val().split(" ~ ");
     var min_price = price_select_val[0]
     var max_price = price_select_val[1]
@@ -71,7 +74,8 @@ $(function(){
     $('.max_price').val(max_price)
   })
 
-  $('#search_sort').change(function(){
+  var search_sort = $('#search_sort')
+  search_sort.change(function(){
     if ($(this).val() != ''){
       current_url = window.location.href
       reg_url     = current_url.replace(/&q%5Bs%5D=(price|created_at)\+(asc|desc)/g, "")
@@ -80,16 +84,23 @@ $(function(){
     }
   })
 
-  $('.btn__clear').click(function(){
-    $("input[type=search]").val('');
-    $("input:checkbox").attr("checked",false);
-    $('.check-box-wrap').empty();
-    $('select').val('');
-    $('.child_select').hide();
+  var clear_btn = $('.btn__clear')
+  var search_input = $("input[type=search]")
+  var checkboxes = $("input:checkbox")
+  var checkbox_wrap = $('.check-box-wrap')
+  var selects = $('select')
+  clear_btn.click(function(){
+    search_input.val('');
+    checkboxes.attr("checked",false);
+    checkbox_wrap.empty();
+    selects.val('');
+    child_select.hide();
   })
 
-  $('.search-bar__button').click(function(){
-    $('.search-bar').toggle();
+  var searchbar_button = $('.search-bar__button')
+  var searchbar = $('.search-bar')
+  searchbar_button.click(function(){
+    searchbar.toggle();
   })
 });
 
