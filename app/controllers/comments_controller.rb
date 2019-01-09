d7a2d03a1f055443afebe8638a0657f89135637f
hiroshimuto <hiroshi.github@gmail.com>
2019-01-09 17:41:13 +0900

    set form to view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,27 @@ class CommentsController < ApplicationController
   before_action :set_product
 
   def index
-
+    @comment = Comment.new
+    @comment = @product.messages.includes(:user)
   end
 
   def create
+    @comment = @product.comments.new(comment_params)
+    if @comment.save
+      redirect_to product_comments_path(@product), notice: 'コメントしました'
+    else
+      @comments = @product.comments.includes(:user)
+      flash.now[:alert] = 'コメントを入力してください。'
+      render :index
+    end
 
   end
 
   private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+  end
 
   def set_product
     @product = Product.find(params[:product_id])
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,7 @@ class ProductsController < ApplicationController
     else @product.brand_id.present? || @product.category_id.present?
       @related_items = Product.where("brand_id = ? or category_id = ?", @product.brand_id, @product.category_id)
     end
+    @comment = Comment.new
   end
 
   def destroy
