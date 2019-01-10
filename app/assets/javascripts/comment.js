3998a776fa13c10a5b9b53f9229cdd63d12116cc
hiroshimuto <hiroshi.github@gmail.com>
2019-01-10 12:28:46 +0900

    delete comment out

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -28,14 +28,9 @@ $(function(){
   }
 
   $('#post-comment').on('submit', function(e){
-    // 同期通信を止める
     e.preventDefault();
-    // FormDataを使って、formから送信されたデータを変数に格納
     var formData = new FormData(this);
-
-    // attr メソッドを使ってform のaction属性を取得し、変数urlに代入
     var url = $(this).attr('action')
-    // ajax通信
     $.ajax({
       url: url,
       type: 'POST',
@@ -44,32 +39,27 @@ $(function(){
       processData: false,
       contentType: false
     })
-    // ajax通信に成功したら
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.text-input').val('');
       $('#post-btn').prop('disabled', false);
     })
-    // ajax通信に失敗した場合
     .fail(function() {
       alert('error');
     });
   });
 
   if (window.location.href.match(/\/products\/\d+/)){
-      // 自動更新機能
     var interval = setInterval(function(){
     var LastCommentId = $('.comment-box').last().data('comment-id');
     var url = window.location.href + '/comments';
-      // ajax通信開始(idが最新のものを取得)
       $.ajax({
         url: url,
         type: "GET",
         data: { id: LastCommentId },
         dataType: "json"
       })
-      // ajax通信に成功したら
       .done(function(comments){
         var id = LastCommentId;
         comments.forEach(function(comment){
@@ -80,13 +70,9 @@ $(function(){
         })
         $('#post-btn').prop('disabled', false);
       })
-      // 自動更新に失敗しましたに失敗した場合
       .fail(function() {
         alert('自動更新に失敗しました(>.<)');
       });
     }, 5000);
   }
-
-
-
 });
