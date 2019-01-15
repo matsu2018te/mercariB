6faf5d68400b9a407094e891196d63d2eb4db0b1
hiroshimuto <hiroshi.github@gmail.com>
2019-01-15 16:56:21 +0900

    delete comment out

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -64,7 +64,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def user_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :telephone, address_attributes: [ :first_name, :first_name, :last_name, :first_name_phonetic, :last_name_phonetic, :id, :postal_code, :prefecture, :municipality, :address_number, :building_name],
       credits_attributes: [:id, :card_number, :expiration_month, :expiration_year, :security_code])
-    #  :birth_year,birth_month,birth_day,
   end
 
   def after_sign_up_path_for(resource)
